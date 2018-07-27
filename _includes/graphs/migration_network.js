355eaf542a95cfb14c8f0355167a436857c0174f
Ashley John <31779750+ashleyajohn@users.noreply.github.com>
2018-07-26 20:18:33 -0400

    Update migration_network.js

--- _includes/graphs/migration_network.js
+++ _includes/graphs/migration_network.js
@@ -27,6 +27,13 @@
         {id: 11, label: 'Charleston', color:'#d1dfea'}
   ]);
 
+    var nodesTx = new vis.DataSet([
+{id: 0, label: 'Austin-Round Rock, TX Metro Area', color:'#dda0dd'},
+{id: 1, label: 'Dallas-Fort Worth-Arlington, TX Metro Area', color:'#ffc443'},
+{id: 2, label: 'Houston-The Woodlands-Sugar Land, TX Metro Area', color:'#6bdaff'},
+{id: 3, label: 'San Antonio-New Braunfels, TX Metro Area', color:'#dc4c4c'}
+    ]);
+
   // create an array with edges
   var edgesPhx = new vis.DataSet([
         {from: 1, to: 2, value:2.32035839, arrows:'from'},
@@ -53,6 +60,21 @@
         {from: 1, to: 10, value:0.8141945, arrows:'to'},
         {from: 1, to: 11, value:0.85689126, arrows:'to'}
   ]);
+
+    var edgesTx = new vis.DataSet([
+{from: 0, to: 1, value:0.658468628977},
+{from: 0, to: 2, value:1.07730339393},
+{from: 0, to: 3, value:0.141600590259},
+{from: 1, to: 0, value:0.423112481457},
+{from: 1, to: 2, value:1.36535446959},
+{from: 1, to: 3, value:0.975019650264},
+{from: 2, to: 0, value:0.386166089233},
+      {from: 2, to: 1, value:2.0931657005},
+      {from: 2, to: 3, value:1.03844973957},
+      {from: 3, to: 0, value:0.93807325804},
+      {from: 3, to: 1, value:0.831157592044},
+      {from: 3, to: 2, value:0.460712792129},
+    ]);
   
   // create a network
   var containerPhx = document.getElementById('mynetworkPhx');
@@ -66,6 +88,12 @@
     nodes: nodesAnc,
     edges: edgesAnc
   };
+
+  var containerTx = document.getElementById('mynetworkTx');
+  var dataTx = {
+        nodes: nodesTx,
+        edges: edgesTx
+    };
   
   var options = {
       locale: 'en',
@@ -83,8 +111,24 @@
       }
   };
 
+var optionsTx = {
+  "edges": {
+    "smooth": {
+      "forceDirection": "none"
+    }
+  },
+  "physics": {
+    "forceAtlas2Based": {
+      "springLength": 100
+    },
+    "minVelocity": 0.75,
+    "solver": "forceAtlas2Based"
+  }
+}
+
   var networkPhx = new vis.Network(containerPhx, dataPhx, options);
   var networkAnc = new vis.Network(containerAnc, dataAnc, options);
+  var networkTx = new vis.Network(containerTx, dataTx, optionsTx);
   
   window.onresize = function() {
   setInterval(function() {
