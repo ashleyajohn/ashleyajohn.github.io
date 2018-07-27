da6932d67163de0e72cd960fd708337692d23d9c
Ashley John <31779750+ashleyajohn@users.noreply.github.com>
2018-07-26 21:37:45 -0400

    Update migration_network.js

--- _includes/graphs/migration_network.js
+++ _includes/graphs/migration_network.js
@@ -34,6 +34,14 @@
 {id: 3, label: 'San Antonio-New Braunfels, TX Metro Area', color:'#dc4c4c'}
     ]);
 
+    var nodesEast = new vis.DataSet([
+{id: 0, label: 'Boston-Cambridge-Newton, MA-NH Metro Area', color:'#ff8e8e'},
+{id: 1, label: 'Chicago-Naperville-Elgin, IL-IN-WI Metro Area', color:'#d1dfea'},
+{id: 2, label: 'New York-Newark-Jersey City, NY-NJ-PA Metro Area', color:'#a0db8e'},
+{id: 3, label: 'Philadelphia-Camden-Wilmington, PA-NJ-DE-MD Metro Area', color:'#dda0dd'}, 
+{id: 4, label: 'Washington-Arlington-Alexandria, DC-VA-MD-WV Metro Area', color:'#6bdaff'}
+    ]);
+
   // create an array with edges
   var edgesPhx = new vis.DataSet([
         {from: 1, to: 2, value:2.32035839, arrows:'from'},
@@ -75,6 +83,30 @@
       {from: 3, to: 1, value:0.831157592044},
       {from: 3, to: 2, value:0.460712792129},
     ]);
+
+    var edgesEast = new vis.DataSet([
+{from: 0, to: 1, value:0.785331824824},
+{from: 0, to: 2, value:1.06476351656},
+{from: 0, to: 3, value:0.710426763015},
+{from: 0, to: 4, value:0.540771274415},
+{from: 1, to: 0, value:0.773543976452},
+{from: 1, to: 2, value:0.0731368845535},
+{from: 1, to: 3, value:0.85934160701},
+{from: 1, to: 4, value:0.672526592299},
+{from: 2, to: 0, value:0.435232728164},
+{from: 2, to: 1, value:0.211606799611},
+{from: 2, to: 3, value:1.56477718613},
+{from: 2, to: 4, value:0.333021637847},
+{from: 3, to: 0, value:0.755041530905},
+{from: 3, to: 1, value:0.933052962656},
+{from: 3, to: 2, value:3.02766409047},
+{from: 3, to: 4, value:0.351121207563},
+{from: 4, to: 0, value:0.373055558332},
+{from: 4, to: 1, value:0.554648108575},
+{from: 4, to: 2, value:1.29276139394},
+{from: 4, to: 3, value:0.124615462889}
+
+    ]);
   
   // create a network
   var containerPhx = document.getElementById('mynetworkPhx');
@@ -93,6 +125,14 @@
   var dataTx = {
         nodes: nodesTx,
         edges: edgesTx
+    };
+
+   var containerEast = document.getElementById('mynetworkEast');
+
+    // provide the data in the vis format
+    var dataEast = {
+        nodes: nodesEast,
+        edges: edgesEast
     };
   
   var options = {
@@ -130,6 +170,7 @@ var optionsTx = {
   var networkPhx = new vis.Network(containerPhx, dataPhx, options);
   var networkAnc = new vis.Network(containerAnc, dataAnc, options);
   var networkTx = new vis.Network(containerTx, dataTx, optionsTx);
+  var networkEast = new vis.Network(containerEast, dataEast, optionsTx);
   
   window.onresize = function() {
   setInterval(function() {
